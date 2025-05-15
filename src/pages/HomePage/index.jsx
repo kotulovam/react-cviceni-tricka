4c4a509b0b9f6fe797cfbe836d226c4423ed3df4
Michaela Kotuľová <kotulova.michaela271@gmail.com>
2025-05-15 11:54:36 +0200

    Solution

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import { TShirt } from '../../components/TShirt';
 import { TShirtSetup } from '../../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+  const [type, setType] = useState('normalShortSleeve');
+  const [color, setColor] = useState('yellow');
+  const [print, setPrint] = useState('Ahoj světe');
+
+  const handleTypeChange = (typeValue) => {
+    setType(typeValue);
+  };
+
+  const handleColorChange = (colorValue) => {
+    setColor(colorValue);
+  };
+
+  const handlePrintChange = (printValue) => {
+    setPrint(printValue);
+  };
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={type} color={color} print={print} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onTypeChange={handleTypeChange}
+            onColorChange={handleColorChange}
+            onPrintChange={handlePrintChange}
+          />
         </div>
       </div>
     </div>
